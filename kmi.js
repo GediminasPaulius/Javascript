5f7e6a5488346631497da9a831c466724fbc1df1
GediminasPaulius <36435951+GediminasPaulius@users.noreply.github.com>
2018-02-14 12:23:52 +0200

    Add files via upload

---
+++ kmi.js
@@ -0,0 +1,48 @@
+angular.module('kmiApp', [])
+    .controller('KmiController', function ($scope)
+{    
+    $scope.Reset = function ()
+    {
+        $scope.Height = "";
+        $scope.Weight = "";
+        
+        $scope.Value = "--";
+        $scope.Classification = "--";
+    }
+
+    $scope.Reset();
+
+    $scope.Calculate = function ()
+    {
+        $scope.Value =  $scope.Weight / Math.pow(Ugiscm($scope.Height), 2);
+
+        
+            var val = $scope.Value;
+
+            if (val < 18.5)
+                $scope.Classification = "Reikia daugiau valgyti";
+                
+            else if (val >= 18.5 & val <= 25)
+                $scope.Classification = "Normalus";
+                
+            
+        
+            else if  (val > 30)
+                $scope.Classification= "Nutukimas";
+                
+            
+            else $scope.Value = $scope.Classification = window.alert("Prašome užpildyti laukelius teisingai");
+            
+        
+    };
+    
+    
+      
+    function Ugiscm( Height)
+    {
+         return Height / 100;
+
+            
+        
+    }
+  });
